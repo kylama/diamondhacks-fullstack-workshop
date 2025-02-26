53d3b9fd67a0029083003124a0940185ce4d45d6
Kyla <kyla123ma@gmail.com>
2025-02-25 19:23:46 -0800

    finished workshop

--- server/models/heistModel.js
+++ server/models/heistModel.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Creating a schema–a "blueprint" for our data
 // TODO: Name each of the fields for the heist and their types
-  // HINT: Look at server/models/crewModel.js if you're stuck!
+// HINT: Look at server/models/crewModel.js if you're stuck!
 const Heistschema = new mongoose.Schema({
-  // ...
+  location: { type: String },
+  startDate: { type: Date },
+  endDate: { type: Date },
+  planEntry: { type: String },
 });
 
 // Create a model for our Heists based on the schema
-const Heists = mongoose.model('Heists', Heistschema);
+const Heists = mongoose.model("Heists", Heistschema);
 
 module.exports = Heists;
--- server/routes/heistRoutes.js
+++ server/routes/heistRoutes.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const heistController = require('../controllers/heistController');
-const crewController = require('../controllers/crewController');
+const heistController = require("../controllers/heistController");
+const crewController = require("../controllers/crewController");
 
 // Add Heist API routes here
+router.get("/heist", heistController.getHeist);
+router.post("/heist", heistController.postHeist);
 
 // Crew API routes
-router.get('/crew', crewController.getCrew);
-router.post('/crew', crewController.postCrew);
-
+router.get("/crew", crewController.getCrew);
+router.post("/crew", crewController.postCrew);
 
 module.exports = router;
